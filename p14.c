cf54fc861bce2c0f9c3a37f9db0fe1d11f1d375e
Pavangowda-B-N <96160480+Pavangowda-B-N@users.noreply.github.com>
2022-03-26 08:16:14 +0000

    p14 gcd

--- p14.c
+++ p14.c
@@ -14,5 +14,25 @@ int input()
 int gcd(int a,int b)
 {
   int hcf=0;
-  for(int i=1;i<=n)
+  for(int i=1;i<=a+1 && i<=b+1 ;i++)
+    {
+      if(a%i==0 && b%i==0)
+      {
+        hcf=i;
+      }
+    }
+  return hcf;
+}
+void output(int a,int b,int hcf)
+{
+  printf("The greatest comman divisor for %d ad %d is %d",a,b,hcf);
+}
+int main()
+{
+  printf("Enter two numbers=");
+  int a=input();
+  int b=input();
+  int g=gcd(a,b);
+  output(a,b,g);
+  return 0;
 }
